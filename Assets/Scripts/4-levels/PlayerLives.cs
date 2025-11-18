20527188251ec17594083447a5d9c6c40a4bb71d
yousefmasarwa97 <93148482+yousefmasarwa97@users.noreply.github.com>
2025-11-18 12:19:23 +0200

    Update PlayerLives.cs

--- Assets/Scripts/4-levels/PlayerLives.cs
+++ Assets/Scripts/4-levels/PlayerLives.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public static class PlayerLives
 {
-    // How many lives the player can have at full health
     public const int MaxLives = 3;
 
     // The current lives (shared between all scenes)
