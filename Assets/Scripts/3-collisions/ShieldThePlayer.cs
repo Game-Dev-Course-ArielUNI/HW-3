98ae2a672c0cf2d3b89e6ea382fdc85978795481
yousefmasarwa97 <93148482+yousefmasarwa97@users.noreply.github.com>
2025-11-19 00:12:19 +0200

    Update ShieldThePlayer.cs

--- Assets/Scripts/3-collisions/ShieldThePlayer.cs
+++ Assets/Scripts/3-collisions/ShieldThePlayer.cs
@@ -2,24 +2,34 @@
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class ShieldThePlayer : MonoBehaviour {
-    [Tooltip("The number of seconds that the shield remains active")] [SerializeField] float duration;
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Player") {
+public class ShieldThePlayer : MonoBehaviour
+{
+    [Tooltip("The number of seconds that the shield remains active")]
+    [SerializeField] private float duration;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
             Debug.Log("Shield triggered by player");
             var destroyComponent = other.GetComponent<DestroyOnTrigger2D>();
-            if (destroyComponent) {
+            if (destroyComponent)
+            {
                 ShieldTemporarily(destroyComponent);
-                Destroy(this.gameObject);  // Destroy the shield itself - to prevent double-use
+                Destroy(this.gameObject); // Destroy the shield itself - to prevent double-use
             }
-        } else {
+        }
+        else
+        {
             Debug.Log("Shield triggered by " + other.name);
         }
     }
 
-    private async void ShieldTemporarily(DestroyOnTrigger2D destroyComponent) {
+    private async void ShieldTemporarily(DestroyOnTrigger2D destroyComponent)
+    {
         destroyComponent.enabled = false;
-        for (float t = duration; t > 0; t--) {
+        for (float t = duration; t > 0; t--)
+        {
             Debug.Log("Shield: " + t + " seconds remaining!");
             await Awaitable.WaitForSecondsAsync(1);
         }
@@ -33,21 +43,21 @@ public class ShieldThePlayer : MonoBehaviour {
             Debug.Log("Shield triggered by player");
             var destroyComponent = other.GetComponent<DestroyOnTrigger2D>();
             if (destroyComponent) {
-                destroyComponent.StartCoroutine(ShieldTemporarily(destroyComponent));        // co-routine
-                    // NOTE: If you just call "StartCoroutine", then it will not work, 
-                    //       since the present object is destroyed!
-                Destroy(gameObject);  // Destroy the shield itself - prevent double-use
+                destroyComponent.StartCoroutine(ShieldTemporarily(destroyComponent)); // co-routine
+                // NOTE: If you just call "StartCoroutine", then it will not work,
+                //       since the present object is destroyed!
+                Destroy(gameObject); // Destroy the shield itself - prevent double-use
             }
         } else {
             Debug.Log("Shield triggered by "+other.name);
         }
     }
 
-    private IEnumerator ShieldTemporarily(DestroyOnTrigger2D destroyComponent) {   // co-routines
+    private IEnumerator ShieldTemporarily(DestroyOnTrigger2D destroyComponent) { // co-routines
         destroyComponent.enabled = false;
         for (float i = duration; i > 0; i--) {
             Debug.Log("Shield: " + i + " seconds remaining!");
-            yield return new WaitForSeconds(1);       // co-routines
+            yield return new WaitForSeconds(1); // co-routines
         }
         Debug.Log("Shield gone!");
         destroyComponent.enabled = true;
