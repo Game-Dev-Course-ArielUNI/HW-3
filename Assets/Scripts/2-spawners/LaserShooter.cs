643895ad9db1f8a2f67fe720941c089f83c61ade
yousefmasarwa97 <93148482+yousefmasarwa97@users.noreply.github.com>
2025-11-19 00:11:29 +0200

    Update LaserShooter.cs

--- Assets/Scripts/2-spawners/LaserShooter.cs
+++ Assets/Scripts/2-spawners/LaserShooter.cs
@@ -4,18 +4,22 @@
  * This component spawns the given laser-prefab whenever the player clicks a given key.
  * It also updates the "scoreText" field of the new laser.
  */
-public class LaserShooter: ClickSpawner {
+public class LaserShooter : ClickSpawner
+{
     [SerializeField]
     [Tooltip("How many points to add to the shooter, if the laser hits its target")]
-    int pointsToAdd = 1;
+    private int pointsToAdd = 1;
 
     // A reference to the field that holds the score that has to be updated when the laser hits its target.
-    private NumberField scoreField;  
+    private NumberField scoreField;
 
-    private void Start() {
+    private void Start()
+    {
         scoreField = GetComponentInChildren<NumberField>();
         if (!scoreField)
+        {
             Debug.LogError($"No child of {gameObject.name} has a NumberField component!");
+        }
     }
 
     private void AddScore()
@@ -23,12 +27,14 @@ public class LaserShooter: ClickSpawner {
         GAME_STATUS.playerScore += pointsToAdd;
     }
 
-
-    protected override GameObject spawnObject() {
-        GameObject newObject = base.spawnObject();  // base = super
+    protected override GameObject spawnObject()
+    {
+        GameObject newObject = base.spawnObject(); // base = super
         DestroyOnTrigger2D newObjectDestroyer = newObject.GetComponent<DestroyOnTrigger2D>();
         if (newObjectDestroyer)
+        {
             newObjectDestroyer.onHit += AddScore;
+        }
         return newObject;
     }
 }
