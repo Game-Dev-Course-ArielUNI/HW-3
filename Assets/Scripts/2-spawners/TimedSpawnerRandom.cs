8b3691aab9ab8151d153fb6ccde122d50e23acfc
yousefmasarwa97 <93148482+yousefmasarwa97@users.noreply.github.com>
2025-11-19 00:10:48 +0200

    Update TimedSpawnerRandom.cs

--- Assets/Scripts/2-spawners/TimedSpawnerRandom.cs
+++ Assets/Scripts/2-spawners/TimedSpawnerRandom.cs
@@ -5,27 +5,45 @@ using UnityEngine;
 /**
  * This component instantiates a given prefab at random time intervals and random bias from its object position.
  */
-public class TimedSpawnerRandom: MonoBehaviour {
-    [SerializeField] Mover prefabToSpawn;
-    [SerializeField] Vector3 velocityOfSpawnedObject;
-    [Tooltip("Minimum time between consecutive spawns, in seconds")] [SerializeField] float minTimeBetweenSpawns = 0.2f;
-    [Tooltip("Maximum time between consecutive spawns, in seconds")] [SerializeField] float maxTimeBetweenSpawns = 1.0f;
-    [Tooltip("Maximum distance in X between spawner and spawned objects, in meters")] [SerializeField] float maxXDistance = 1.5f;
-    [SerializeField] Transform parentOfAllInstances;
+public class TimedSpawnerRandom : MonoBehaviour
+{
+    [SerializeField] private Mover prefabToSpawn;
+    [SerializeField] private Vector3 velocityOfSpawnedObject;
 
-    void Start() {
-         SpawnRoutine();
+    [Tooltip("Minimum time between consecutive spawns, in seconds")]
+    [SerializeField] private float minTimeBetweenSpawns = 0.2f;
+
+    [Tooltip("Maximum time between consecutive spawns, in seconds")]
+    [SerializeField] private float maxTimeBetweenSpawns = 1.0f;
+
+    [Tooltip("Maximum distance in X between spawner and spawned objects, in meters")]
+    [SerializeField] private float maxXDistance = 1.5f;
+
+    [SerializeField] private Transform parentOfAllInstances;
+
+    private void Start()
+    {
+        SpawnRoutine();
     }
 
-    async void SpawnRoutine() {
-        while (true) {
+    private async void SpawnRoutine()
+    {
+        while (true)
+        {
             float timeBetweenSpawnsInSeconds = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
-            await Awaitable.WaitForSecondsAsync(timeBetweenSpawnsInSeconds);       // co-routines
-            if (!this) break;   // might be destroyed when moving to a new scene
+            await Awaitable.WaitForSecondsAsync(timeBetweenSpawnsInSeconds); // co-routines
+
+            if (!this)
+            {
+                break; // might be destroyed when moving to a new scene
+            }
+
             Vector3 positionOfSpawnedObject = new Vector3(
                 transform.position.x + Random.Range(-maxXDistance, +maxXDistance),
                 transform.position.y,
-                transform.position.z);
+                transform.position.z
+            );
+
             GameObject newObject = Instantiate(prefabToSpawn.gameObject, positionOfSpawnedObject, Quaternion.identity);
             newObject.GetComponent<Mover>().SetVelocity(velocityOfSpawnedObject);
             newObject.transform.parent = parentOfAllInstances;
