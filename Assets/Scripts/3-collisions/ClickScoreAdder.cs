65cbf6b766ea8256f7adf005a162ce0d59829a32
yousefmasarwa97 <93148482+yousefmasarwa97@users.noreply.github.com>
2025-11-19 00:21:57 +0200

    Update ClickScoreAdder.cs

--- Assets/Scripts/3-collisions/ClickScoreAdder.cs
+++ Assets/Scripts/3-collisions/ClickScoreAdder.cs
@@ -8,15 +8,25 @@ public class ClickScoreAdder : MonoBehaviour
     [SerializeField] protected InputAction addAction = new InputAction(type: InputActionType.Button);
     [SerializeField] protected int scoreToAdd;
 
-    void OnEnable()    {        addAction.Enable();    }
-    void OnDisable()    {        addAction.Disable();    }
+    private void OnEnable()
+    {
+        addAction.Enable();
+    }
 
-    private void Start()   {
+    private void OnDisable()
+    {
+        addAction.Disable();
+    }
+
+    private void Start()
+    {
         GetComponent<NumberField>().SetNumber(1);
     }
 
-    private void Update() {
-        if (addAction.WasPressedThisFrame()) {
+    private void Update()
+    {
+        if (addAction.WasPressedThisFrame())
+        {
             GetComponent<NumberField>().AddNumber(scoreToAdd);
         }
     }
